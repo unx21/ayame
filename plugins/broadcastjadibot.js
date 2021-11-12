263ee00c436a1869895765701ed4c9bf736ed398
unxzx <84166927+unx21@users.noreply.github.com>
2021-11-13 00:37:15 +0800

    Update broadcastjadibot.js

--- plugins/broadcastjadibot.js
+++ plugins/broadcastjadibot.js
@@ -1,10 +1,12 @@
 let handler = async (m, { conn, text }) => {
   if (conn.user.jid === global.conn.user.jid) {
     let users = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user.jid)])]
-    let content = await conn.cMod(m.chat, m, /bc|broadcast/i.test(text) ? text : text + '\n' + readMore + '「 ' + conn.getName(conn.user.jid) + ' Broadcast 」')
-    for (let id of users) conn.copyNForward(id, content)
-    conn.reply(m.chat, `_Berhasil mengirim broadcast ke ${users.length} nomor yang jadi bot_
-${users.map(v => 'wa.me/' + v.replace(/[^0-9]/g,'') + '?text=.menu').join('\n')}`.trim(), m)
+    let cc = text ? m : m.quoted ? await m.getQuotedObj() : false || m
+    let teks = text ? text : cc.text
+    let content = conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n\n' + readMore + '「 ' + conn.getName(conn.user.jid) + ' Broadcast 」')
+    for (let id of users) conn.copyNForward(id, content, true)
+  conn.reply(m.chat, `_Berhasil mengirim broadcast ke ${users.length} nomor yang jadi bot_
+${users.map(v => 'wa.me/' + v.replace(/[^0-9]/g, '') + `?text=${usedPrefix}menu`).join('\n')}`.trim(), m)
   } else conn.reply(m.chat, 'Fitur ini hanya untuk host bot',  m)
 }
 handler.command = /^(broadcast|bc)(jadi)?bot$/i
