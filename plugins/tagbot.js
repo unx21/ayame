9c6274f70dc3ec77a443578150a2ff9e3d13e955
unxzx <84166927+unx21@users.noreply.github.com>
2021-11-13 02:11:47 +0800

    figs

--- plugins/tagbot.js
+++ plugins/tagbot.js
@@ -1,13 +1,17 @@
-let handler = async (m, { conn, text }) => {
-    let name = ('6289524339740@s.whatsapp.net')
-    let panggil = `
-    百鬼あやめ\n\nAda Hanya Untuk Melayani Master U n x
-    `.trim()
-    let mentionedJid = [name]
-  conn.reply(m.chat, panggil, m, { contextInfo: { mentionedJid }})
-
+let handler  = async (m, { conn, isOwner }) => {
+if (!isOwner) throw false
+let name = conn.getName(m.sender)
+let me = conn.user.name
+let teks = `
+${pickRandom([` _Saya disini Master_`, ` _Ayamee hadir_`, ` _Apakah Master mencari saya??_`, ` _Yapp Master_`, `*_百鬼あやめ_*\n\nAda hanya untuk melayani Master  U n x`])}
+`.trim()
+conn.fakeReply(m.chat, teks, '0@s.whatsapp.net', `${me}  Love  U n x`, 'status@broadcast')
 }
-handler.customPrefix = /6289524339740/i
+handler.customPrefix = /6289524339740|ayame/
 handler.command = new RegExp
 
 module.exports = handler
+
+function pickRandom(list) {
+    return list[Math.floor(Math.random() * list.length)]
+}
