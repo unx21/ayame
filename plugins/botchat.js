309ee6d60241b6f635382485aae4425abbde1349
unxzx <84166927+unx21@users.noreply.github.com>
2021-11-13 01:05:42 +0800

    Update botchat.js

--- plugins/botchat.js
+++ plugins/botchat.js
@@ -1,10 +1,15 @@
 let handler  = async (m, { conn }) => {
-  
 let name = conn.getName(m.sender)
-let pesan = `_Hai ${name}\n\n_Ada yang bisa saya bantu?_`
-conn.fakeReply(m.chat, pesan, '0@s.whatsapp.net', 'Salam Hangat Dari Owner', 'status@broadcast')
+let teks = `
+${pickRandom([` Hii ${name}\n\nSaya disini`, `Oeetss`, `Ekhheemmm`, `Puuuiyy`, `Poom`, `Hadiirr`, `Panggil Trosss`, `Diemin aja dah`, `Mohon maaf ${name}\n\nSaya sedang sibuk saat ini, Hubungi saya lain waktu`, `Meh`, `Nyahallo`])}
+`.trim()
+conn.fakeReply(m.chat, teks, '0@s.whatsapp.net', 'Ajak Ngomong Lah Biar Gak Stress', 'status@broadcast')
 }
-handler.customPrefix = /^Bot$/
+handler.customPrefix = /Bot/
 handler.command = new RegExp
 
 module.exports = handler
+
+function pickRandom(list) {
+    return list[Math.floor(Math.random() * list.length)]
+}
