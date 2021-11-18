6289321c021bd7f7cb2a8668c2ed288e6a49628b
unxzx <84166927+unx21@users.noreply.github.com>
2021-11-18 20:30:56 +0800

    C

--- plugins/botvn.js
+++ plugins/botvn.js
@@ -6,6 +6,6 @@ let vn = './src/sound/baka.mp3'
 conn.sendFile(m.chat, vn, 'baka.mp3', null, m, {
 type: 'audioMessage', ptt: true })
 }
-handler.customPrefix = /(B|b)aka/
+handler.customPrefix = /Baka/
 handler.command = new RegExp
 module.exports = handler
