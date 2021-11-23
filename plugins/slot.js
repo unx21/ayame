0c98f18b9433540f7a2713e54aaed55b41492949
unxzx <84166927+unx21@users.noreply.github.com>
2021-11-23 12:54:55 +0800

    Yahh capek

--- plugins/slot.js
+++ plugins/slot.js
@@ -1,11 +1,14 @@
 let { MessageType } = require('@adiwajshing/baileys')
 let num = /([0-9])$/i
-let handler = async (m, { conn, text }) => {
-    if (!text) throw 'Masukkan jumlah Uang Yang Akan Anda Slot'
-    if (!num.test(text)) throw 'Hanya Angka'
-    let uang = `${text}`
-    let jackpot = Math.ceil(uang * 5)
-    let win = Math.ceil(uang * 1)
+let handler = async (m, { conn, args, usedPrefix }) => {
+let type = (args[0] || '').toLowerCase()
+let count = args[1] && args[1].length > 0 ? Math.min(9999999, Math.max(parseInt(args[1]), 1)) : Math.min(1)
+switch (type) {
+            case 'uang':
+    if (!args) throw 'Masukkan Jumlah Uang Yang Akan Anda Slot'
+    let uang = `${count}`
+    let jackpot = Math.ceil(uang * 10)
+    let win = Math.ceil(uang * 2)
     if (uang < 1000) throw 'Minimal 1000'
     let users = global.db.data.users
     if (uang > users[m.sender].uang) throw 'Uang Anda Tidak Cukup'
@@ -52,6 +55,62 @@ let handler = async (m, { conn, text }) => {
         global.db.data.users[m.sender].uang -= uang
         await conn.fakeReply(m.chat, `*[ 🎰 VIRTUAL SLOT 🎰 ]*\n\n${gcha}\n\n*[ 🎰 VIRTUAL SLOT 🎰 ]*`, '0@s.whatsapp.net', `${hasil}`, 'status@broadcast')
     }
+    break
+    case 'koin':
+    if (!args) throw 'Masukkan Jumlah Koin Yang Akan Anda Slot'
+    let koin = `${count}`
+    let jakpot = Math.ceil(koin * 5)
+    let won = Math.ceil(koin * 1)
+    if (koin < 1000) throw 'Minimal 1000'
+    let user = global.db.data.users
+    if (koin > user[m.sender].koin) throw 'Koin Anda Tidak Cukup'
+    let emoji = ["🍏","🍎","🍊","🍋","🍑","🪙","🍅","🍐"];
+    let d = Math.floor(Math.random() * emoji.length);
+    let e = Math.floor(Math.random() * emoji.length);
+    let f = Math.floor(Math.random() * emoji.length);
+    let j = [],
+        k = [],
+        l = [];
+    for (let i = 0; i < 3; i++) {
+        j[i] = emoji[d];
+        d++;
+        if (d == emoji.length) d = 0;
+    }
+    for (let i = 0; i < 3; i++) {
+        k[i] = emoji[e];
+        e++;
+        if (e == emoji.length) e = 0;
+    }
+    for (let i = 0; i < 3; i++) {
+        l[i] = emoji[f];
+        f++;
+        if (f == emoji.length) f = 0;
+    }
+    let ends;
+    if (d == e && e == f) {
+        ends = "_*JACKPOT*_";
+        hsil = `Win With 3 Thing Common +${jakpot} Coins`;
+        gacha = `${j[0]} | ${k[0]} | ${l[0]}\n${j[1]} | ${k[1]} | ${l[1]} <=== ${ends}\n${j[2]} | ${k[2]} | ${l[2]}`;
+        global.db.data.users[m.sender].koin += jakpot
+        await conn.fakeReply(m.chat, `*[ 🎰 VIRTUAL SLOT 🎰 ]*\n\n${gacha}\n\n*[ 🎰 VIRTUAL SLOT 🎰 ]*`, '0@s.whatsapp.net', `${hsil}`, 'status@broadcast')
+
+    } else if (d == e || d == f || e == f) {
+        ends = "_*YOU WIN*_";
+        hsil = `Win With 2 Things Common +${won} Coins`;
+        gacha = `${j[0]} | ${k[0]} | ${l[0]}\n${j[1]} | ${k[1]} | ${l[1]} <=== ${ends}\n${j[2]} | ${k[2]} | ${l[2]}`;
+        global.db.data.users[m.sender].koin += won
+        await conn.fakeReply(m.chat, `*[ 🎰 VIRTUAL SLOT 🎰 ]*\n\n${gacha}\n\n*[ 🎰 VIRTUAL SLOT 🎰 ]*`, '0@s.whatsapp.net', `${hsil}`, 'status@broadcast')
+    } else {
+        ends = "_*YOU LOSE*_";
+        hsil = `Hopefully You Are lucky Next -${koin} Coins`;
+        gacha = `${j[0]} | ${k[0]} | ${l[0]}\n${j[1]} | ${k[1]} | ${l[1]} <=== ${ends}\n${j[2]} | ${k[2]} | ${l[2]}`;
+        global.db.data.users[m.sender].koin -= koin
+        await conn.fakeReply(m.chat, `*[ 🎰 VIRTUAL SLOT 🎰 ]*\n\n${gacha}\n\n*[ 🎰 VIRTUAL SLOT 🎰 ]*`, '0@s.whatsapp.net', `${hsil}`, 'status@broadcast')
+    }
+ break
+ default:
+       return conn.reply(m.chat, `Masukkan Pilihan Uang Atau Koin\nContoh Penggunaan:  ${usedPrefix}slot ${pickRandom(['uang', 'koin'])} 10000`, m)
+  }
 }
 //handler.help = ['slot']
 //handler.tags = ['game']
@@ -63,3 +122,7 @@ handler.exp = 10
 handler.limit = true
 
 module.exports = handler
+
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
+}
