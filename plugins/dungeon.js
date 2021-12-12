d83f82d25531222db39974a7b956e215bbd10f48
unxzx <84166927+unx21@users.noreply.github.com>
2021-12-12 16:42:07 +0800

    Update dungeon.js

--- plugins/dungeon.js
+++ plugins/dungeon.js
@@ -4,20 +4,25 @@ let handler = async (m, { conn, usedPrefix, DevMode }) => {
         let __timers = (new Date - global.db.data.users[m.sender].lastdungeon)
         let _timers = (300000 - __timers) 
         let timers = clockString(_timers)
-        
-        
+
+       
+        if (global.db.data.users[m.sender].swordpower > 49) {
         if (global.db.data.users[m.sender].durability > 19) {
         if (global.db.data.users[m.sender].healt > 89) {
             if (new Date - global.db.data.users[m.sender].lastdungeon > 300000) {
             let healt = (pickRandom(['100', '99', '100', '97', '100', '98', '100', '96', '100', '95', '100', '94', '100', '93', '100', '92', '100', '91', '100', '90', '100']))
             let durability = (pickRandom(['15', '14', '13', '12', '11', '10', '9', '8', '7']))
+            let swordpower = (pickRandom(['50', '49', '48', '47', '46', '45', '44', '43', '42', '41']))
             let koin = `${Math.floor(Math.random() * 350)}`.trim() 
             let lantai = `${Math.floor(Math.random() * 99)}`.trim()
             let slime = `${Math.floor(Math.random() * 10)}`.trim() 
-            let sprtgrass = `${Math.floor(Math.random() * 5)}`.trim() 
-            let mdgrleaf = `${Math.floor(Math.random() * 5)}`.trim() 
-            let hpktplant = `${Math.floor(Math.random() * 5)}`.trim() 
+            let sprtgrass = `${Math.floor(Math.random() * 15)}`.trim()
+            let _mdgrleaf = `${pickRandom(['1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1'])}`
+            let mdgrleaf = (_mdgrleaf * 1)
+            let _hpktplant = `${pickRandom(['1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1'])}`
+            let hpktplant = (_hpktplant * 1)
             let kayu = `${Math.floor(Math.random() * 20)}`.trim() 
+            let sword = global.db.data.users[m.sender].sword
             let _skeleton = `${pickRandom(['1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'])}`
             let skeleton = (_skeleton * 1)
             let goblin = `${Math.floor(Math.random() * 7)}`.trim() 
@@ -25,7 +30,7 @@ let handler = async (m, { conn, usedPrefix, DevMode }) => {
             let wolf = (_wolf * 1)
             let sampah = `${Math.floor(Math.random() * 100)}`.trim()
             let str = `
-Kamu memasuki dungeon hingga lantai ${lantai}\nNyawamu berkurang -${healt * 1} dan Durability berkurang -${durability * 1} setelah masuk dungeon dan mendapatkan
+Kamu berhasil masuk hingga lantai ${lantai} dan mendapatkan:\n
 
 *「 REWARD 」*
 
@@ -36,6 +41,10 @@ Kamu memasuki dungeon hingga lantai ${lantai}\nNyawamu berkurang -${healt * 1} d
 🌱 *Hipokute Plant: ${hpktplant}*
 🪵 *Kayu: ${kayu}*
 ♻️ *Sampah: ${sampah}*
+\n\n𝘒𝘦𝘳𝘶𝘴𝘢𝘬𝘢𝘯:\n
+𝘔𝘦𝘯𝘦𝘳𝘪𝘮𝘢 𝘥𝘢𝘮𝘢𝘨𝘦 𝘴𝘦𝘣𝘦𝘴𝘢𝘳 _${healt * 1}_
+𝘒𝘦𝘩𝘪𝘭𝘢𝘯𝘨𝘢𝘯 𝘥𝘶𝘳𝘢𝘣𝘪𝘭𝘪𝘵𝘺 𝘴𝘦𝘣𝘦𝘴𝘢𝘳 _${durability * 1}_
+𝘚𝘸𝘰𝘳𝘥 𝘗𝘰𝘸𝘦𝘳 𝘣𝘦𝘳𝘬𝘶𝘳𝘢𝘯𝘨 𝘴𝘦𝘣𝘢𝘯𝘺𝘢𝘬 _${swordpower * 1}_
 `.trim()
             conn.reply(m.chat, str, m)
             if (goblin > 0) {
@@ -52,17 +61,20 @@ Kamu memasuki dungeon hingga lantai ${lantai}\nNyawamu berkurang -${healt * 1} d
             }
             global.db.data.users[m.sender].healt -= healt * 1
             global.db.data.users[m.sender].durability -= durability * 1
+            global.db.data.users[m.sender].swordpower -= swordpower * 1
             global.db.data.users[m.sender].koin += koin * 1
             global.db.data.users[m.sender].kayu += kayu * 1
             global.db.data.users[m.sender].slime += slime * 1
-            global.db.data.users[m.sender].sprtgrass += sprtgrass * 1
-            global.db.data.users[m.sender].mdgrleaf += mdgrleaf * 1
-            global.db.data.users[m.sender].hpktplant += hpktplant * 1
+            global.db.data.users[m.sender].spiritgrass += sprtgrass * 1
+            global.db.data.users[m.sender].mandragoraleaf += mdgrleaf * 1
+            global.db.data.users[m.sender].hipokuteplant += hpktplant * 1
             global.db.data.users[m.sender].sampah += sampah * 1
+            global.db.data.users[m.sender].sword -= sword
             global.db.data.users[m.sender].lastdungeon = new Date * 1
             } else conn.reply(m.chat, `Memasuki dungeon membuat anda lelah, silahkan coba *${timers}* lagi`, m)
-        } else conn.reply(m.chat, 'Minimal 90 health untuk bisa masuk Dungeon lagi, Silahkan racik potion terlebih dahulu', m)
-      } else conn.reply(m.chat, 'Kamu perlu 20 durability untuk dapat masuk dungeon', m)
+          } else conn.reply(m.chat, 'Minimal 90 health untuk bisa masuk Dungeon lagi, Silahkan racik potion terlebih dahulu', m)
+        } else conn.reply(m.chat, 'Kamu perlu 20 durability untuk dapat masuk dungeon', m)
+      } else conn.reply(m.chat, 'Sword kamu sudah tidak layak untuk dipakai, Silahkan tempa sword kamu kembali', m)
       if (global.db.data.users[m.sender].healt < 0) {   // Jika healt user kurang dari 0, maka level dan exp akan di kurangi sebagaimana mestinya  :v
         	let exp = `${Math.floor(Math.random() * 1000)}`.trim()
             let _level = `${pickRandom(['1', '1', '1', '1', '1'])}`
@@ -72,7 +84,13 @@ Kamu memasuki dungeon hingga lantai ${lantai}\nNyawamu berkurang -${healt * 1} d
             global.db.data.users[m.sender].exp -= exp * 1
             global.db.data.users[m.sender].level -= level * 1
             }
-    } catch (e) {
+            if (global.db.data.users[m.sender].swordpower < 10) {
+                let sword = global.db.data.users[m.sender].sw,ord
+                let vok = `Sword kamu ${pickRandom(['patah', 'rusak', 'hancur'])}\nSilahkan tempa lagi`.trim()
+                conn.reply(m.chat, vok, m)
+                global.db.data.users[m.sender].sword -= sword
+                }
+   } catch (e) {
         console.log(e)
         conn.reply(m.chat, 'Error', m)
         if (DevMode) {
@@ -85,12 +103,14 @@ Kamu memasuki dungeon hingga lantai ${lantai}\nNyawamu berkurang -${healt * 1} d
 }
 handler.command = /^dungeon$/i
 handler.register = true
+
 handler.fail = null
 
 module.exports = handler
 
+
 function pickRandom(list) {
-    return list[Math.floor(Math.random() * list.length)]
+return list[Math.floor(Math.random() * list.length)]
 }
 function clockString(ms) {
   let h = Math.floor(ms / 3600000)
