4a2d0a29342e5e14b24f6c0d4184f968af1b54f9
unxzx <84166927+unx21@users.noreply.github.com>
2021-12-12 16:48:12 +0800

    Update racik.js

--- plugins/racik.js
+++ plugins/racik.js
@@ -3,54 +3,53 @@ let { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn, command, args, usedPrefix, DevMode }) => {
   let type = (args[0] || '').toLowerCase()
   let rck = (args[0] || '').toLowerCase()
-  let sprtgrass = global.db.data.users[m.sender].sprtgrass
-  let mdgrleaf = global.db.data.users[m.sender].mdgrleaf
-  let hpktplant = global.db.data.users[m.sender].hpktplant
+  let spiritgrass = global.db.data.users[m.sender].spiritgrass
+  let mandragoraleaf = global.db.data.users[m.sender].mandragoraleaf
+  let hipokuteplant = global.db.data.users[m.sender].hipokuteplant
   let bruh = `
 Tanaman Herbal yang bisa di racik
 
-🌿 *sprtgrass*
-🍂 *mdgrleaf*
-🌱 *hpktplant*
+🌿 *spiritgrass*
+🍂 *mandragoraleaf*
+🌱 *hipokuteplant*
 
 
 *[ Stok Tanaman Herbal Kamu ]*
 
-🌿 sprtgrass  ${sprtgrass} tanaman
-🍂 mdgrleaf  ${mdgrleaf} tanaman
-🌱 hpktplant  ${hpktplant} tanaman
+🌿 spiritgrass  ${spiritgrass} tanaman
+🍂 mandragoraleaf  ${mandragoraleaf} tanaman
+🌱 hipokuteplant  ${hipokuteplant} tanaman
 
 
 contoh:
-*${usedPrefix + command } sprtgrass 1*
+*${usedPrefix + command } spiritgrass 1*
 `.trim()
 try {
        if (/racik/i.test(command)) {
             const count = args[1] && args[1].length > 0 ? Math.min(99999999, Math.max(parseInt(args[1]), 1)) : !args[1] || args.length < 3 ? 1 : Math.min(1, count)
             switch (type) {
-            	case 'sprtgrass':
-            if (global.db.data.users[m.sender].sprtgrass >= count * 1) {
-                           global.db.data.users[m.sender].sprtgrass >= count * 1
-                            global.db.data.users[m.sender].sprtgrass -= count * 1
+            	case 'spiritgrass':
+            if (global.db.data.users[m.sender].spiritgrass >= count * 1) {
+                           global.db.data.users[m.sender].spiritgrass >= count * 1
+                            global.db.data.users[m.sender].spiritgrass -= count * 1
                             global.db.data.users[m.sender].spotion += count * 1
                             conn.reply(m.chat, `${count} Spirit Grass diracik menjadi ${count} Small Potion`, m)
                        } else conn.reply(m.chat, `Tanaman herbal kamu tidak cukup`, m)
 break
-
-                  case 'mdgrleaf':
-            if (global.db.data.users[m.sender].mdgrleaf >= count * 1) {
-                            global.db.data.users[m.sender].mdgrleaf >= count * 1
-                            global.db.data.users[m.sender].mdgrleaf -= count * 1
-                            global.db.data.users[m.sender].spotion += count * 1
-                            conn.reply(m.chat, `${count} Mandragora Leaf diracik menjadi ${count} Small Potion`, m)
+                  case 'mandragoraleaf':
+            if (global.db.data.users[m.sender].mandragora >= count * 1) {
+                            global.db.data.users[m.sender].mandragora >= count * 1
+                            global.db.data.users[m.sender].mandragora -= count * 1
+                            global.db.data.users[m.sender].mpotion += count * 1
+                            conn.reply(m.chat, `${count} Mandragora Leaf diracik menjadi ${count} Medium Potion`, m)
                        } else conn.reply(m.chat, `Tanaman herbal kamu tidak cukup`, m)
 break
-                   case 'hpktplant':
-            if (global.db.data.users[m.sender].hpktplant >= count * 1) {
-                           global.db.data.users[m.sender].hpktplant >= count * 1
-                            global.db.data.users[m.sender].hpktplant -= count * 1
-                            global.db.data.users[m.sender].spotion += count * 1
-                            conn.reply(m.chat, `${count} Hipokute Plant diracik menjadi ${count} Small Potion`, m)
+                   case 'hipokuteplant':
+            if (global.db.data.users[m.sender].hipokuteplant >= count * 1) {
+                           global.db.data.users[m.sender].hipokuteplant >= count * 1
+                            global.db.data.users[m.sender].hipokuteplant -= count * 1
+                            global.db.data.users[m.sender].hpotion += count * 1
+                            conn.reply(m.chat, `${count} Hipokute Plant diracik menjadi ${count} High Potion`, m)
                        } else m.reply('Tanaman herbal kamu tidak cukup')
             break
         default:
@@ -58,7 +57,7 @@ break
     }
   }} catch (e) {
       console.log(e)
-      conn.reply(m.chat, `*${usedPrefix + command }sprtgrass 1*`, m)
+      conn.reply(m.chat, `*${usedPrefix + command }spiritgrass 1*`, m)
       if (DevMode) {
         for (let jid of global.owner.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)) {
             conn.sendMessage(jid, 'racik.js error\nNo: *' + m.sender.split`@`[0] + '*\nCommand: *' + m.text + '*\n\n*' + e + '*', MessageType.text)
