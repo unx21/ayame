c7649cd2b3c4f26ee63c717e1bafa325a0f6478d
unxzx <84166927+unx21@users.noreply.github.com>
2021-12-18 00:04:44 +0800

    Update leave.js

--- plugins/leave.js
+++ plugins/leave.js
@@ -3,13 +3,13 @@ let handler = async function(m, { conn, args, isPrems, isOwner }) {
 const time = async (ms) => {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-      this.fakeReply(m.chat, `*Bye Semuaa 🙂*`, '0@s.whatsapp.net', `${conn.user.name} Verified Bot`, 'status@broadcast')
+      this.fakeReply(m.chat, `_Bye Semuaa, stay happy_ 🙂`, '0@s.whatsapp.net', `${conn.user.name} Verified WhatsApp Bot`, 'status@broadcast')
       await time(5000)
   await conn.groupLeave(m.key.remoteJid)
 }
 //handler.help = ['leavegroup', 'keluargrup']
 //handler.tags = ['owner']
-handler.command = /^(leave|keluar)gro?up$/i
+handler.command = /^leaveg(c|ro?up)(all|semua)?$/i
 handler.owner = true
 handler.mods = false
 handler.premium = false
