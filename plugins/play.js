acdfc0ced4c0f901c378a0df25a1b73e4cdce959
unxzx <84166927+unx21@users.noreply.github.com>
2021-12-24 08:33:33 +0800

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -4,6 +4,7 @@ let fetch = require('node-fetch')
 const { servers, yta, ytv } = require('../lib/y2mate')
 let handler = async (m, { conn, command, text, isPrems, isOwner }) => {
   if (!text) throw 'Cari apa?'
+  await m.reply(global.wait)
   let chat = global.db.data.chats[m.chat]
   let results = await yts(text)
   let vid = results.all.find(video => video.seconds < 3600)
