9451bf82e4ecc7dcb2618b85db98207e6e2dce11
lynx46 <86021822+lynx46@users.noreply.github.com>
2022-01-15 11:10:03 +0800

    Update wallpaper.js

--- plugins/wallpaper.js
+++ plugins/wallpaper.js
@@ -6,9 +6,7 @@ let handler = async (m, { conn }) => {
       await m.reply(global.wait)
         let res = await fetch(global.API('xteam', '/randomimage/wpmobile', {}, 'APIKEY'))
         let img = await res.buffer()
-        conn.sendMessage(m.chat, img, MessageType.image, {
-            quoted: m, caption: '*©Kuriyama-bot*'
-        })
+        await conn.sendButtonImg(m.chat, await(await fetch(img)).buffer(), 'Random Wallpaper', '© シエル', 'Next', m.text, m)
     } catch (e) {
         console.log(e)
         throw '_*Owner belum membayar tagihan fitur ini*_'
