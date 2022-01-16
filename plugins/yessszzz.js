c3e2e044ff312c48224dc5db7236698194d429fc
unxzx <84166927+unx21@users.noreply.github.com>
2022-01-17 00:30:31 +0800

    Create yessszzz.js

---
+++ plugins/yessszzz.js
@@ -0,0 +1,23 @@
+let fetch = require("node-fetch")
+let handler = async (m, { conn }) => {
+await m.reply(global.wait)
+let res = await fetch('https://raw.githubusercontent.com/unx21/ayame/zx/src/wallpaper.txt')
+let txt = await res.text()
+
+let arr = txt.split('\n')
+let cita = arr[Math.floor(Math.random() * arr.length)]
+await conn.sendButtonImg(m.chat, await(await fetch(cita)).buffer(), `Enjoy These High Quality Wallpapers`, '© 赤井はあと', 'Next', m.text, m)
+}
+
+handler.help = ['random', 'wpo', 'wpp', 'wphd']
+handler.tags = ['OWNER ONLY']
+handler.command = /^random|wpo|wpp|wphd$/i
+handler.limit = false
+handler.owner = true
+handler.register = true
+
+module.exports = handler
+
+//Made with 💙 by unx//
+
+module.exports = handler
