d03e932d878ab0600b126a1588a9abc028b24563
unxzx <84166927+unx21@users.noreply.github.com>
2022-01-23 23:52:41 +0800

    Update semoji.js

--- plugins/semoji.js
+++ plugins/semoji.js
@@ -6,6 +6,7 @@ const { sticker } = require('../lib/sticker')
 const fetch = require('node-fetch')
 const defaultType = 'whatsapp'
 let handler = async (m, { usedPrefix, conn, args, text }) => {
+  await m.reply(global.wait)
   let [tipe, emoji] = text.includes('|') ? text.split('|') : args
   if (tipe && !emoji) {
     emoji = tipe
