558305c50cee62cf9a35b80c1b3e13a14d6461a9
unxzx <84166927+unx21@users.noreply.github.com>
2022-01-25 09:07:41 +0800

    Create igstory2.js

---
+++ plugins/igstory2.js
@@ -0,0 +1,36 @@
+// thanks to https://github.com/qisyana //
+
+let handler = async (m, { usedPrefix, command, conn, args }) => {
+  if (!args[0]) throw `Gunakan format: ${usedPrefix}${command} instagram`
+  let res = await igstory(args[0])
+  if (!res.length) throw 'User no have story!'
+  for (let { url, type } of res)
+    conn.sendFile(m.chat, url, 'ig' + (type == 'video' ? '.mp4' : '.jpg'), `
+@${args[0]}
+`.trim(), m)
+}
+//handler.help = ['igstory'].map(v => v + ' <username>')
+//handler.tags = ['downloader']
+
+handler.command = /^(igstory2)$/i
+handler.register = true
+handler.premium = true
+
+module.exports = handler
+
+const axios = require('axios')
+const cheerio = require('cheerio')
+const fetch = require('node-fetch')
+async function igstory(username) {
+  username = username.replace(/https:\/\/instagram.com\//g, '')
+  let { data } = await axios.get(`https://www.instadownloader.org/data.php?username=${username}&t=${new Date * 1}`)
+  const $ = cheerio.load(data)
+  let results = []
+  $('body > center').each(function (i, el) {
+    results.push({
+      url: $(el).find('a.download-btn').attr('href'),
+      type: $(el).find('video').html() ? 'video' : 'image'
+    })
+  })
+  return results
+}
