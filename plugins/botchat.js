103e96fb90fc10b5079aececae016552080db2b1
unxzx <84166927+unx21@users.noreply.github.com>
2022-01-26 09:54:24 +0800

    Ngebugg

--- plugins/botchat.js
+++ plugins/botchat.js
@@ -1,9 +1,8 @@
 let handler  = async (m, { conn }) => {
-let name = conn.getName(m.sender)
 let teks = `
-${pickRandom([` _Hii @{name}_\n\n_Saya disini_`, `_Oeetss ${name}_\n\n_Ada apa??_`, `_Ekhheemmm, Uhuk Ughhuk_`, `_Puuuiyy_`, `_Poom_`, `_Hadirrr_`, `_Panggil Trosss_`, `_Diemin aja dah_`, `_Mohon maaf ${name}_\n\n_Saya sedang sibuk saat ini, Hubungi saya lain waktu_`, `Meh`, `Nyenyenyenye`, `Aku Diam Aku Aman`])}
+${pickRandom([` _Hii @${m.sender.split`@`[0]}_\n\n_Saya disini_`, `_Oeetss @${m.sender.split`@`[0]}_\n\n_Ada apa??_`, `_Ekhheemmm, Uhuk Ughhuk_`, `_Puuuiyy_`, `_Poom_`, `_Hadirrr_`, `_Panggil Trosss_`, `_Diemin aja dah_`, `_Mohon maaf @${m.sender.split`@`[0]}_\n\n_Saya sedang sibuk saat ini, Hubungi saya lain waktu_`, `Meh`, `Nyenyenyenye`, `Aku Diam Aku Aman`])}
 `.trim()
-conn.reply(m.chat, teks, m)
+conn.reply(m.chat, teks, m, { contextInfo: { mentionedJid: [m.sender] }})
 }
 handler.customPrefix = /Bot/
 handler.command = new RegExp
