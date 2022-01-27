3ee359fe8c3b7d0b8545acc08f5e511c528efe15
unxzx <84166927+unx21@users.noreply.github.com>
2022-01-27 21:19:35 +0800

    Aaarrrgggvhhh

--- plugins/register.js
+++ plugins/register.js
@@ -34,7 +34,7 @@ let handler = async function (m, { text, usedPrefix }) {
   let foot = `_Simpan serial number anda!_\n_Jika sn kamu hilang silahkan ketik ${usedPrefix}ceksn_`
   global.db.data.users[m.sender].uang += 10000
   global.db.data.users[m.sender].koin += 5000
-  await conn.sendButtonImg(m.chat, pp, '.jpg', caption, foot, 'P r o f i l e', `.profile`, m.text, m)
+  await conn.sendButtonImg(m.chat, await(await fetch(pp)).buffer(), caption, foot, 'P r o f i l e', `.profile`, m.text, m)
 }
 //handler.help = ['daftar', 'reg', 'register'].map(v => v + ' <nama>.<umur>')
 //handler.tags = ['exp']
