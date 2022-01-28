8ee89c872d28800d2ed18569099dc14438336c98
unxzx <84166927+unx21@users.noreply.github.com>
2022-01-28 17:58:57 +0800

    Update register.js

--- plugins/register.js
+++ plugins/register.js
@@ -13,8 +13,7 @@ let handler = async function (m, { text, usedPrefix }) {
   user.regTime = + new Date
   user.registered = true
   let sn = createHash('md5').update(m.sender).digest('hex')
-  let caption = `
-┏ ┅ ━━━━━━━━━━━━━━━━━━━━━ ┅ ━
+  let caption = `┏ ┅ ━━━━━━━━━━━━━━━━━━━━━ ┅ ━
 ┇       *「 INFORMATION 」*
 ┣ ┅ ━━━━━━━━━━━━━━━━━━━━━ ┅ ━
 ┃
@@ -24,21 +23,9 @@ let handler = async function (m, { text, usedPrefix }) {
 ┃ *Gift:* Rp10000 and 5000 Coins
 ┃ *Serial Number:* 
 ┃ ${sn}
-┗ ┅ ━━━━━━━━━━━━━━━━━━━━━ ┅ ━
-
- _Simpan Serial Number anda!_
- _*jika sn kamu hilang, ketik ${usedPrefix}ceksn_
-`.trim()
-await conn.fakeReply(m.chat, caption,/* { 
-  key: { 
-    remoteJid: 'status@broadcast',
-    participant:*/ '0@s.whatsapp.net', 
-    /*fromMe: false 
-  }, message: { 
-    "imageMessage": { 
-      "mimetype": "image/jpeg", 
-      "caption": */` *Registration Successful!!*`, /*
-      "jpegThumbnail": tnbot} } }, { contextInfo: { mentionedJid: [m.sender] } }m*/'status@broadcast')
+┗ ┅ ━━━━━━━━━━━━━━━━━━━━━ ┅ ━`
+   let foot = `_Simpan Serial Number anda!_\nJika sn kamu hilang silahkan ketik ${usedPrefix}ceksn_`
+  conn.sendButton(m.chat, caption, foot, 'P r o f i l e', '.profile', m.text, m)
 global.db.data.users[m.sender].uang += 10000
 global.db.data.users[m.sender].koin += 5000
 }
