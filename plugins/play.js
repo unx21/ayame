22607ffa6705d40145f035c030658c3e3f0f2f56
unxzx <84166927+unx21@users.noreply.github.com>
2022-01-30 19:31:24 +0800

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -1,45 +1,42 @@
-let limit = 30
-let { MessageType } = require('@adiwajshing/baileys')
-let yts = require('yt-search')
+//Thanks to Beniismael > _ <
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 const { servers, yta, ytv } = require('../lib/y2mate')
-let handler = async (m, { conn, command, text, isPrems, isOwner, DevMode }) => {
-  conn.play = conn.play ? conn.play : {}
-  if (m.chat in conn.play) throw 'Masih ada yang sedang mencari di youtube\ndi chat ini... tunggu sampai selesai'
-  else conn.play[m.chat] = true
-  try {
-      try {
-          if (!text) throw 'Cari apa?'
-          await m.reply(global.wait)
-          let results = await yts(text)
-          let vid = results.all.find(video => video.seconds < 3600)
-          if (!vid) throw 'Video/Audio Tidak ditemukan'
-          let { dl_link, thumb, title, filesize, filesizeF} = await (/2$/.test(command) ? ytv : yta)(vid.url, 'id4')
-          let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
-          conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
-*Source:* ${vid.url}
-*${isLimit ? 'Pakai ': ''}Link:* ${dl_link}
-`.trim(), m)
-          if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp' + (3 + /2$/.test(command)), `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
-*Source:* ${vid.url}
-`.trim(), m)
-      } catch (e) {
-          console.log(e)
-          m.reply('error')
-          if (DevMode) {
-              for (let jid of global.owner.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)) {
-                  conn.sendMessage(jid, 'Play.js error\nNo: *' + m.sender.split`@`[0] + '*\nCommand: *' + m.text + '*\n\n*' + e + '*', MessageType.text)
-              }
-          }
-      }
-  } finally {
-    delete conn.play[m.chat]
+let yts = require('yt-search')
+let fetch = require('node-fetch')
+let handler = async (m, { conn, command, text, usedPrefix }) => {
+  if (!text) throw `uhm.. cari apa?\n\ncontoh:\n${usedPrefix + command} powfu death bed`
+  await m.reply(global.wait)
+  let chat = global.db.data.chats[m.chat]
+  let results = await yts(text)
+  let vid = results.all.find(video => video.seconds < 3600)
+  if (!vid) throw 'Konten Tidak ditemukan'
+  let isVideo = /2$/.test(command)
+  let yt = false
+  let yt2 = false
+  let usedServer = servers[0]
+  for (let i in servers) {
+    let server = servers[i]
+    try {
+      yt = await yta(vid.url, server)
+      yt2 = await ytv(vid.url, server)
+      usedServer = server
+      break
+    } catch (e) {
+      m.reply(`Server ${server} error!${servers.length >= i + 1 ? '' : '\nmencoba server lain...'}`)
+    }
   }
+  if (yt === false) throw 'semua server gagal'
+  if (yt2 === false) throw 'semua server gagal'
+  let { dl_link, thumb, title, filesize, filesizeF } = yt
+  await conn.send2ButtonLoc(m.chat, await (await fetch(thumb)).buffer(), `
+*Judul:* ${title}
+*Ukuran File Audio:* ${filesizeF}
+*Ukuran File Video:* ${yt2.filesizeF}
+*Server y2mate:* ${usedServer}
+`.trim(), '© 百鬼あやめ', 'Audio', `.yta ${vid.url}`, 'Video', `.yt ${vid.url}`)
 }
-//handler.help = ['play', 'play2'].map(v => v + ' <pencarian>')
+//handler.help = ['play'].map(v => v + ' <pencarian>')
 //handler.tags = ['downloader']
 handler.command = /^play2?$/i
 handler.register = true
@@ -48,4 +45,3 @@ handler.premium = true
 handler.exp = 0
 
 module.exports = handler
-
