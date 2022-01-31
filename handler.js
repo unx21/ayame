e5f41300b760210bad84dab219418115ea994f30
unxzx <84166927+unx21@users.noreply.github.com>
2022-01-31 23:14:18 +0800

    Update handler.js

--- handler.js
+++ handler.js
@@ -546,9 +546,7 @@ Untuk mematikan fitur ini, ketik
 }
 
 global.dfail = (type, m, conn) => {
-  let teks = 'Anda perlu mendaftar terlebih dahulu dengan cara mengetik #verify'
-  let foot = 'Tekan tombol verifikasi di bawah jika anda malas untuk mengetik'
-  let msg = {
+    let msg = {
     rowner: '_Perintah ini hanya dapat digunakan oleh Master_',
     owner: '_Perintah ini hanya dapat digunakan oleh Owner Bot_',
     mods: '_Perintah ini hanya dapat digunakan oleh Moderator_',
@@ -557,7 +555,7 @@ global.dfail = (type, m, conn) => {
     private: '_Perintah ini hanya dapat digunakan di Chat Pribadi_',
     admin: '_Perintah ini hanya untuk *Admin* grup_',
     botAdmin: '_Jadikan bot sebagai *Admin* untuk menggunakan perintah ini_',
-    unreg: conn.sendButton(m.chat, teks, foot, 'V e r i f y', ` .verify`, m.text, m)
+    unreg: await conn.sendButton(m.chat, 'Anda perlu mendaftar terlebih dahulu dengan cara mengetik #verify', 'Tekan tombol verifikasi di bawah jika anda malas untuk mengetik', 'V e r i f y', '.verify', m.text, m)
   }[type]
   if (msg) return m.reply(msg)
 }
