129b4a57986a1cf990e94ca6ad5cff17ba01100b
unxzx <84166927+unx21@users.noreply.github.com>
2022-02-12 17:45:40 +0800

    Update link.js

--- plugins/link.js
+++ plugins/link.js
@@ -13,7 +13,7 @@ let handler = async (m, { conn, args }) => {
 //handler.help = ['linkgroup']
 //handler.tags = ['group']
 handler.command = /^link(gro?up)?$/i
-handler.register = true
+handler.register = false
 handler.owner = false
 handler.mods = false
 handler.premium = false
