7b9e6371f38300919226bdd749e7f4640bc451ad
unxzx <84166927+unx21@users.noreply.github.com>
2022-02-12 11:35:40 +0800

    Update absen-delete.js

--- plugins/absen-delete.js
+++ plugins/absen-delete.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn, usedPrefix, isAdmin, isOwner }) => {
     let id = m.chat
     conn.absen = conn.absen ? conn.absen : {}
     if (!(id in conn.absen)) {
-        await conn.sendButton(m.chat, `Tidak ada absen berlangsung!`, '© stikerin', 'Mulai', `${usedPrefix}mulaiabsen`, m)
+        await conn.sendButton(m.chat, `Tidak ada absen berlangsung!`, '© 百鬼あやめ', 'Mulai', `${usedPrefix}mulaiabsen`, m)
         throw false
     }
     delete conn.absen[id]
@@ -17,5 +17,5 @@ let handler = async (m, { conn, usedPrefix, isAdmin, isOwner }) => {
 //handler.help = ['hapusabsen']
 //handler.tags = ['absen']
 handler.command = /^(delete|hapus|-)absen$/i
-handler.register = true
+handler.register = false
 module.exports = handler
