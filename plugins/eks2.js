f8f1ce6430109fd6875800c3f4ed26c2f86e0de7
unxzx <84166927+unx21@users.noreply.github.com>
2022-02-12 12:12:39 +0800

    Update eks2.js

--- plugins/eks2.js
+++ plugins/eks2.js
@@ -19,7 +19,7 @@ let stiker = await sticker(null, url, command.charAt(0).toUpperCase() + command.
 }
 
 handler.command = /^(pat|hug|wink|facepalm)/i
-handler.register = true
+handler.register = false
 handler.limit = true
 
 module.exports = handler
