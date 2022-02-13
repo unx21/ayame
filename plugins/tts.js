a052c269d8097c642cb70632942b15b238f272bb
unxzx <84166927+unx21@users.noreply.github.com>
2022-02-14 01:20:19 +0800

    Update tts.js

--- plugins/tts.js
+++ plugins/tts.js
@@ -25,7 +25,7 @@ let handler = async (m, { conn, args }) => {
 //handler.help = ['tts <lang> <teks>']
 //handler.tags = ['tools']
 handler.command = /^g?tts$/i
-handler.register = true
+handler.register = false
 
 module.exports = handler
 
