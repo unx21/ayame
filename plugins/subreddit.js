ce063111ee9e348489ae019874a71f1fdbb8d27f
unxzx <84166927+unx21@users.noreply.github.com>
2022-02-13 16:33:48 +0800

    Update subreddit.js

--- plugins/subreddit.js
+++ plugins/subreddit.js
@@ -6,13 +6,12 @@ let handler = async (m, { conn, text }) => {
   if (!res.ok) throw await res.text()
   let json = await res.json()
   if (!json.url) throw 'Media tidak ditemukan!'
-  if (json.nsfw) throw 'Content blocked'
   await conn.sendFile(m.chat, json.url, text, json.title, m, false, { thumbnail: Buffer.alloc(0) })
 }
 //handler.help = ['subreddit <query>']
 //handler.tags = ['internet']
 handler.command = /^(sr|subreddit)$/i
-handler.register = true
-handler.limit = true
+handler.register = false
+handler.limit = 3
 
 module.exports = handler
