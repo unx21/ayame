bf5d93dc0347fc7a9a13f909ac8a8b6b1dbac419
unxzx <84166927+unx21@users.noreply.github.com>
2022-02-14 01:04:46 +0800

    Update tictactoe.js

--- plugins/tictactoe.js
+++ plugins/tictactoe.js
@@ -41,7 +41,7 @@ Ketik *nyerah* untuk nyerah
         })
         await this.sendButton(room.o, str, author, 'Nyerah', 'nyerah', m, {
             contextInfo: {
-                mentionedJid: this.parseMention(str)
+                mentionedJid: conn.parseMention(str)
             }
         })
     } else {
