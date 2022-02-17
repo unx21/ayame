0c7f1819296d102b454227d9b07e2f75a64f7ecc
unxzx <84166927+unx21@users.noreply.github.com>
2022-02-17 13:10:30 +0800

    Create pinterest2.js

---
+++ plugins/pinterest2.js
@@ -0,0 +1,26 @@
+let fetch = require('node-fetch')
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+  if (!text) throw `Contoh: ${usedPrefix + command} minecraft`
+  let res = await fetch(global.API('zeks', '/api/pinimg', {
+    q: encodeURI(text)
+  }, 'apikey'))
+  await m.reply(global.wait)
+  if (!res.ok) throw await `${res.status} ${res.statusText}`
+  let json = await res.json()
+  if (!json.status) throw json
+  let pint = json.data[Math.floor(Math.random() * json.data.length)];
+  /* conn.sendFile(m.chat, pint, '', `
+*Hasil pencarian*
+${text}
+`.trim(), m) */
+
+   await conn.sendButtonImg(m.chat, `*Hasil Pencarian*\n${text}`, '© 百鬼あやめ', 'N e x t', `#pint2 ${text}`, m)
+}
+//handler.help = ['pinterest <keyword>']
+//handler.tags = ['internet']
+handler.command = /^(pint(erest)2?)$/i
+handler.register = true
+handler.premium = true
+
+
+module.exports = handler
