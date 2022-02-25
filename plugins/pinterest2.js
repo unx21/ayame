6c993b37e4a57c1cf66824070b962ebf8d2aa102
unxzx <84166927+unx21@users.noreply.github.com>
2022-02-25 11:16:54 +0800

    Update pinterest2.js

--- plugins/pinterest2.js
+++ plugins/pinterest2.js
@@ -1,18 +1,25 @@
 let fetch = require('node-fetch')
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) throw `uhm.. cari apa?\n\ncontoh:\n${usedPrefix + command} logo`
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+  if (!text) throw `Contoh: ${usedPrefix + command} minecraft`
   let res = await fetch(global.API('zeks', '/api/pinimg', {
     q: encodeURI(text)
   }, 'apikey'))
-  if (!res.ok) throw eror
+  await m.reply(global.wait)
+  if (!res.ok) throw await `${res.status} ${res.statusText}`
   let json = await res.json()
   if (!json.status) throw json
   let pint = json.data[Math.floor(Math.random() * json.data.length)];
-  conn.sendButtonImg(m.chat, await(await fetch(pint)), `Hasil Pencarian\n${text}`, '© 百鬼あやめ', 'N e x t', `#pint2 ${text}`, m)
+  /* conn.sendFile(m.chat, pint, '', `
+*Hasil pencarian*
+${text}
+`.trim(), m) */
+await conn.sendButtonImg(m.chat, await(await fetch(pint)).buffer(), `Hasil pencarian\n${text}`, '© 百鬼あやめ', 'Next', m.text, m)
 }
-//handler.help = ['pinterest <pencarian>']
+//handler.help = ['pinterest <keyword>']
 //handler.tags = ['internet']
 handler.command = /^(pint2)$/i
+handler.register = false
 handler.premium = true
+
 
 module.exports = handler
