ed43c5a0ab32edce968e1f4c702982b44fcd11a8
unxzx <84166927+unx21@users.noreply.github.com>
2022-02-25 10:59:20 +0800

    Update igstory.js

--- plugins/igstory.js
+++ plugins/igstory.js
@@ -2,7 +2,7 @@ const { igstory } = require('../lib/scrape')
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
-  if (!args[0]) throw `uhm.. username nya mana?\n\ncontoh:\n\n${usedPrefix + command} unxzx_`
+  if (!args[0]) throw `Pengunaan:\n${usedPrefix + command} <url>\n\nContoh:\n\n${usedPrefix + command} unxzx_`
   if (args[0].startsWith('http') || args[0].startsWith('@')) throw `username salah`
 
   igstory(args[0]).then(async res => {
@@ -10,15 +10,15 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     let json = JSON.parse(igs)
     await m.reply(global.wait)
     for (let { downloadUrl, type } of json)
-      conn.sendFile(m.chat, downloadUrl, 'ig' + (type == 'image' ? '.jpg' : '.mp4'), '©Violet-bot', m)
+      conn.sendFile(m.chat, downloadUrl, 'ig' + (type == 'image' ? '.jpg' : '.mp4'), '© Violet-bot', m)
 
   })
 
 }
+//handler.help = ['igstory'].map(v => v + ' <username>')
+//handler.tags = ['downloader']
 handler.command = /^(igs(tory)?)$/i
 
-handler.limit = false
 handler.premium = true
-handler.register = false
 
 module.exports = handler
