7bb518d6600e2c1ce82df289df8d92133954f7e3
unxzx <84166927+unx21@users.noreply.github.com>
2022-02-26 05:33:31 +0800

    Update tempa.js

--- plugins/tempa.js
+++ plugins/tempa.js
@@ -13,6 +13,7 @@ Resource yang bisa di tempa
 *~ magicarmor*
 *~ peluru*
 *~ sword*
+*~ highsword*
 
 
 contoh:
@@ -25,7 +26,8 @@ _Biaya Penempaan_
 - _goldarmor = 75 gold_
 - _magicarmor = 170 mgstone_
 - _peluru = 1 mgstone_
-- _sword = 5 iron + 2 gold_
+- _sword = 5 iron_
+- _highsword = 10 iron + 3 gold_
 
 
 *[ Stok Item Kamu ]*
@@ -36,7 +38,7 @@ _Biaya Penempaan_
 `.trim()
 try {
 
-       if (/tempa/i.test(command)) {
+       if (/tempa|menempa/i.test(command)) {
        	const count = args[1] && args[1].length > 0 ? Math.min(99999999, Math.max(parseInt(args[1]), 1)) : !args[1] || args.length < 3 ? 1 : Math.min(1, count)
             switch (type) {
             	case 'ironarmor':
@@ -64,15 +66,24 @@ break
                        } else conn.reply(m.chat, `Kamu tidak memiliki stok magic stone yang cukup`, m)
 break
                 case 'sword':
-            if (global.db.data.users[m.sender].gold >= count * 2) {
             if (global.db.data.users[m.sender].iron >= count * 5) {
-                            global.db.data.users[m.sender].gold >= count * 2
                             global.db.data.users[m.sender].iron >= count * 5
-                            global.db.data.users[m.sender].gold -= count * 2
                             global.db.data.users[m.sender].iron -= count * 5
                             global.db.data.users[m.sender].sword += count * 1
                             global.db.data.users[m.sender].swordpower += count * 100
-                            conn.reply(m.chat, `${count * 2} Gold dan ${count * 5} Iron di tempa menjadi ${count} Sword dan mendapatkan ${count * 100} Sword Power`, m)
+                            conn.reply(m.chat, `${count * 5} Iron di tempa menjadi ${count} Sword dan mendapatkan ${count * 100} Sword Power`, m)
+                       } else conn.reply(m.chat, `Kamu tidak memiliki stok iron yang cukup`, m)
+break
+                 case 'highsword':
+             if (global.db.data.users[m.sender].gold >= count * 3) {
+             if (global.db.data.users[m.sender].iron >= count * 10) {
+                            global.db.data.users[m.sender].gold >= count * 3
+                            global.db.data.users[m.sender].iron >= count * 10
+                            global.db.data.users[m.sender].gold -= count * 3
+                            global.db.data.users[m.sender].iron -= count * 10
+                            global.db.data.users[m.sender].highsword += count * 1
+                            global.db.data.users[m.sender].swordpower += count * 275
+                            conn.reply(m.chat, `${count * 3} Gold dan ${count * 10} Iron di tempa menjadi ${count} Sword dan mendapatkan ${count * 275} Sword Power`, m)
                        } else conn.reply(m.chat, `Kamu tidak memiliki stok iron yang cukup`, m)
                      } else conn.reply(m.chat, `Kamu tidam memiliki stok gold yang cukup`, m)
 break
