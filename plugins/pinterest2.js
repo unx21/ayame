06e27c886548f4b7c42b956ffcc853c7385b02eb
unxzx <84166927+unx21@users.noreply.github.com>
2022-02-25 11:09:53 +0800

    Update pinterest2.js

--- plugins/pinterest2.js
+++ plugins/pinterest2.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
   let json = await res.json()
   if (!json.status) throw json
   let pint = json.data[Math.floor(Math.random() * json.data.length)];
-  conn.sendButtonImg(m.chat, await(await fetch(pint)), `Hasil Pencarian\n${text}` '© 百鬼あやめ', 'N e x t', `#pint2 ${text}`, m)
+  conn.sendButtonImg(m.chat, await(await fetch(pint)), `Hasil Pencarian\n${text}`, '© 百鬼あやめ', 'N e x t', `#pint2 ${text}`, m)
 }
 //handler.help = ['pinterest <pencarian>']
 //handler.tags = ['internet']
