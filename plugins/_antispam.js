7e8243a2b6b3784edd0333a3f3febcfcc152e31a
unxzx <84166927+unx21@users.noreply.github.com>
2022-03-01 14:13:34 +0800

    Update _antispam.js

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -3,12 +3,13 @@ module.exports = {
         if (!m.message) return
         this.spam = this.spam ? this.spam : {}
         if (m.isBaileys && m.fromMe) return true
+        if (!isOwner) return true
         if (m.sender in this.spam) {
             this.spam[m.sender].count++
             if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 10) {
                 if (this.spam[m.sender].count > 10) {
                     global.db.data.users[m.sender].banned = true
-                    m.reply('*Terdeteksi Spam, Kamu Telah Di Ban Karena Melanggar Rules No.6\nTerimaKasih')
+                    m.reply('*Terdeteksi Spam, Kamu Telah Di Ban Karena Melanggar Rules No.6*\nTerimaKasih')
                 }
                 this.spam[m.sender].count = 0
                 this.spam[m.sender].lastspam = m.messageTimestamp.toNumber()
