fdad9caa12e0672bf82afb4cb37c52ba5c887152
unxzx <84166927+unx21@users.noreply.github.com>
2022-03-01 11:50:03 +0800

    Update tiktok2.js

--- plugins/tiktok2.js
+++ plugins/tiktok2.js
@@ -3,6 +3,7 @@
 const { tiktokdl, tiktokdlv2 } = require('@bochilteam/scraper')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
+    await m.reply(global.wait)
     const { author: { nickname }, video, description } = await tiktokdl(args[0]).catch(async _ => await tiktokdlv2(args[0]))
     const url = video.no_watermark || video.no_watermark_hd || video.with_watermark || video.no_watermark_raw
     if (!url) throw 'Can\'t download video!'
