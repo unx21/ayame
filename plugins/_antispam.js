60807749e46ad1129f8620e58a568f560ad5306e
unxzx <84166927+unx21@users.noreply.github.com>
2022-03-01 16:30:17 +0800

    Update _antispam.js

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -3,8 +3,8 @@ module.exports = {
         if (!m.message) return
         this.spam = this.spam ? this.spam : {}
         if (m.isBaileys && m.fromMe) return true
-        if (m.isOwner) return
         if (m.sender in this.spam) {
+           if (m.isOwner) return
             this.spam[m.sender].count++
             if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 10) {
                 if (this.spam[m.sender].count > 10) {
