7f2f6a40bd8e80186fcbb54da8e31dd0b8790f60
unxzx <84166927+unx21@users.noreply.github.com>
2022-03-04 11:28:00 +0800

    Update absen-start.js

--- plugins/absen-start.js
+++ plugins/absen-start.js
@@ -8,11 +8,11 @@ let handler = async (m, { conn, usedPrefix, text, isAdmin, isOwner }) => {
     conn.absen = conn.absen ? conn.absen : {}
     let id = m.chat
     if (id in conn.absen) {
-        await conn.send2Button(m.chat, `Masih ada absen di chat ini!`, '© stikerin', 'Hapus', `${usedPrefix}hapusabsen`, 'Cek', `${usedPrefix}cekabsen`, m)
+        await conn.send2Button(m.chat, `Masih ada absen di chat ini!`, '© 百鬼あやめ', 'Hapus', `${usedPrefix}hapusabsen`, 'Cek', `${usedPrefix}cekabsen`, m)
         throw false
     }
     conn.absen[id] = [
-        await conn.sendButton(m.chat, `Absen dimulai`, '© stikerin', 'Absen', `${usedPrefix}absen`, m),
+        await conn.sendButton(m.chat, `Absen dimulai`, '© 百鬼あやめ', 'Absen', `${usedPrefix}absen`, m),
         [],
         text
     ]
@@ -20,6 +20,7 @@ let handler = async (m, { conn, usedPrefix, text, isAdmin, isOwner }) => {
 //handler.help = ['mulaiabsen [teks]']
 //handler.tags = ['absen']
 handler.command = /^(start|mulai)absen$/i
+handler.admin = true
 handler.register = false
 
 module.exports = handler
