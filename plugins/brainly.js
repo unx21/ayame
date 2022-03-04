c4bbc6b0ed5f612d68b9c2cd3b81a59dcbca5c2a
unxzx <84166927+unx21@users.noreply.github.com>
2022-03-04 09:34:14 +0800

    Update brainly.js

--- plugins/brainly.js
+++ plugins/brainly.js
@@ -1,20 +1,50 @@
-const Brainly = require('brainly-scraper-v2')
-const brain = new Brainly('id')
+const { Brainly } = require('brainly-scraper-v2')
+const brainly = new Brainly('id')
+let handler = async function (m, { text }) {
+  if (!text) throw 'Soalnya?'
+  let res = await brainly.search('id', text)
+  let answer = res.map(({ question, answers }, i) => `
+_*PERTANYAAN KE ${i + 1}*_
+${formatTags(question.content)}${answers.map((v, i) => `
+*JAWABAN KE ${i + 1}*${v.verification ? ' (Verified)' : ''}${v.isBest ? ' (Terpilih)' : ''}
+${formatTags(v.content)}`).join``}`).join(`
 
-let handler = async function (m, { text, usedPrefix, command }) {
-  if (!text) throw `Pengunaan:\n${usedPrefix + command} <soal>\n\nContoh:\n${usedPrefix + command} apa itu javascript?`
-  await m.reply(global.wait)
-  brain.search('id', text).then(res => {
-    let json = JSON.parse(JSON.stringify(res))
-    let answer = json.map((v, i) => `_*PERTANYAAN KE ${i + 1}*_\n${v.question.content}\n${v.answers.map((v, i) => `*JAWABAN KE ${i + 1}*\n${v.content.replace(/<\/?p>|<\/?strong>|<\/?u>|<\/?h[1-3]>|<\/?span>/g, '').replace(/<br ?(\/|\\)?>/g, '\n')}`).join('\n')}`).join('\n\n•------------•\n\n')
-    m.reply(answer)
-  })
+•------------•
+
+`)
+  m.reply(answer)
 }
 //handler.help = ['brainly <soal>']
 //handler.tags = ['internet']
 
 handler.command = /^brainly$/i
-handler.register = false
 handler.limit = true
 
 module.exports = handler
+
+function formatTags(str) {
+  let tagRegex = /<(.+?)>((?:.|\n)*?)<\/\1>/gi
+  let replacer = (_, tag, inner) => {
+    let a = inner.replace(tagRegex, replacer)
+    let b = ''
+    switch (tag) {
+      case 'p':
+        a += '\n'
+        break
+      case 'i':
+        b = '_'
+      case 'strikethrough':
+        b = '~'
+      case 'strong':
+        b = '*'
+        a = a.split('\n').map(a => a ? b + a + b : a).join('\n')
+        break
+    }
+    return a
+  }
+  
+  return str
+    .replace(/<br *?\/?>/gi, '\n')
+    .replace(tagRegex, replacer)
+    .trim()
+}
