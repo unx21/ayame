567de33d680b41cfe46c51694453e47d45700efc
unxzx <84166927+unx21@users.noreply.github.com>
2022-03-04 11:46:59 +0800

    Update vote-start.js

--- plugins/vote-start.js
+++ plugins/vote-start.js
@@ -2,9 +2,9 @@ let handler = async (m, { conn, text, usedPrefix }) => {
     conn.vote = conn.vote ? conn.vote : {}
     let id = m.chat
     if (id in conn.vote) {
-        throw `_Masih ada vote di chat ini!_\n\n*${usedPrefix}hapusvote* - untuk menghapus vote`
+        conn.send2Button(m.chat, `Masih ada vote yang berlangsung di chat ini!`, '© 百鬼あやめ', 'Cek', `${usedPrefix}cekvote`, 'Hapus', `${usedPrefix}hapusvote`, m)
     }
-    m.reply(`Vote dimulai!\n\n*${usedPrefix}upvote* - untuk ya\n*${usedPrefix}devote* - untuk tidak\n*${usedPrefix}cekvote* - untuk mengecek vote\n*${usedPrefix}hapusvote* - untuk menghapus vote`)
+    conn.send2Button(m.chat, `Vote dimulai!\n\n*${usedPrefix}upvote* - untuk ya\n*${usedPrefix}devote* - untuk tidak`, '© 百鬼あやめ', 'Upvote', `${usedPrefix}upvote`, 'Devote', `${usedPrefix}devote`, m)
     conn.vote[id] = [
         text,
         [],
@@ -15,8 +15,7 @@ let handler = async (m, { conn, text, usedPrefix }) => {
 //handler.tags = ['vote']
 handler.command = /^(start|mulai)vote$/i
 handler.register = false
-
-handler.limit = true
 handler.group = true
 handler.admin = true
+
 module.exports = handler
