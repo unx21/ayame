ef7436d41fc4a5970370ee7d42aedc304f3e2862
unxzx <84166927+unx21@users.noreply.github.com>
2022-03-04 11:59:59 +0800

    Update vote-cekvote.js

--- plugins/vote-cekvote.js
+++ plugins/vote-cekvote.js
@@ -18,7 +18,7 @@ ${upvote.map(u => '@' + u.split('@')[0]).join('\n')}
 _Total: ${devote.length}_
 ${devote.map(u => '@' + u.split('@')[0]).join('\n')}
 `.trim()
-conn.send3Button(m.chat, caption, '© 百鬼あやめ', 'Upvote', `${usedPrefix}upvote`, 'Devote', `${usedPrefix}devote`, 'Hapus', `${usedPrefix}hapusvote`, m, { contextInfo: { mentionedJid } })
+conn.send3Button(m.chat, caption, '© 百鬼あやめ', 'Upvote', `${usedPrefix}upvote`, 'Devote', `${usedPrefix}devote`, 'Hapus', `${usedPrefix}hapusvote`,m, { contextInfo: { mentionedJid: conn.parseMention(caption) } })
 }
 //handler.help = ['cekvote']
 //handler.tags = ['vote']
