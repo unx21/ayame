f136bf32ce4405b6b1eeae30d79af8c932c9230e
unxzx <84166927+unx21@users.noreply.github.com>
2022-03-05 11:50:31 +0800

    Update trigger.js

--- plugins/trigger.js
+++ plugins/trigger.js
@@ -18,6 +18,6 @@ let handler = async (m, { conn }) => {
 //handler.tags = ['maker']
 handler.command = /^(trigger)$/i
 handler.register = false
-handler.limit = true
+handler.limit = 1
 
 module.exports = handler
