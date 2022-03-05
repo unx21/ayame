3b7d367ff7c474beaca10eabb7494b119b4b701d
unxzx <84166927+unx21@users.noreply.github.com>
2022-03-05 10:27:17 +0800

    Update layarkaca.js

--- plugins/layarkaca.js
+++ plugins/layarkaca.js
@@ -40,7 +40,7 @@ handler.premium = false
 handler.group = false
 handler.private = false
 handler.register = false
-handler.limit = true
+handler.limit = 1
 
 handler.admin = false
 handler.botAdmin = false
