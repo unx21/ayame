ff5751c1c23524097c2261c51be0e88229a6ce9a
unxzx <84166927+unx21@users.noreply.github.com>
2022-03-06 18:43:53 +0800

    Update asahotak_hint.js

--- plugins/asahotak_hint.js
+++ plugins/asahotak_hint.js
@@ -1,12 +1,13 @@
 let handler = async (m, { conn }) => {
     conn.asahotak = conn.asahotak ? conn.asahotak : {}
     let id = m.chat
-    if (!(id in conn.asahotak)) throw false
+    if (!(id in conn.asahotak)) throw 0
     let json = conn.asahotak[id][1]
-    let nya = json.jawaban
-    let nyanya = nya.replace(/[bcdfghjklmnpqrstvwxyz]/g, '_')
-    m.reply('```' + nyanya + '```')
+    let clue = json.jawaban.replace(/[AIUEOaiueo]/g, '_')
+    conn.reply(m.chat, '```' + clue + '```', conn.asahotak[id][0])
 }
 handler.command = /^aohint$/i
+
 handler.limit = true
+
 module.exports = handler
