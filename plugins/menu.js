8493ca4490d646a274ab3985a0dc8ac5ca368f1c
unxzx <84166927+unx21@users.noreply.github.com>
2022-03-20 12:45:31 +0800

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -184,8 +184,7 @@ const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
 
 function clockString(ms) {
-  let z = isNaN(ms) ? '--' : Math.floor(ms / 216000000) 
-  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 60
+  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) 
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
   return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
