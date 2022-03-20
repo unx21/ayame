0c92db147bea11cb79c6931470a3e28206e8ecb6
unxzx <84166927+unx21@users.noreply.github.com>
2022-03-20 12:34:52 +0800

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -71,7 +71,6 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
       year: 'numeric'
     }).format(d)
     let time = d.toLocaleTimeString(locale, {
-      day: 'numeric',
       hour: 'numeric',
       minute: 'numeric',
       second: 'numeric'
@@ -185,7 +184,8 @@ const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
 
 function clockString(ms) {
-  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+  let d = isNaN(ms) ? '--' : Math.floor(ms / 216000000) 
+  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 60
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
   return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
